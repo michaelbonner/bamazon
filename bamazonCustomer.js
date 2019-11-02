ed8b1a091c9bb374c3afcc6b7710b2a36223cbff
tworoose <51271998+kspaffk@users.noreply.github.com>
2019-11-02 11:15:14 -0600

    fix stock issue

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,48 +11,68 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+// var to store the products from the DB
 var products;
+// var to identify if this is the first time the program is running
+var runFirstTime = true;
 
-var getProducts = function() {
-    console.log('\n')
+// gets products from the database and updates the products variable
+function getProducts() {
     var queryProducts = 'SELECT * FROM products';
     connection.query(queryProducts, function(error, results) {
         if (error) {
-            console.log('\nThere was a connection error.')
+            console.log('Connection error occured!');
         } else {
             products = results;
-            console.log(chalk.magentaBright.underline('ID:\t|\tProduct      \t|\tPrice'))
-            results.forEach(product => {
-                if (product.stock_quantity > 0) {
-                    var divider = '\t|\t';
-                    var lg_divider = '     \t|\t';
-                    var list = "";
-                    if (product.product_name.length < 8) {
-                        list = product.item_id +  divider + product.product_name + lg_divider + '$' + product.price;
-                    } else if (product.product_name.length > 11) {
-                        list = product.item_id +  divider + product.product_name.substring(0, 11) + divider + '$' + product.price;
-                    } else {
-                        list = product.item_id +  divider + product.product_name + divider + '$' + product.price;
-                    }
-
-                    if (product.item_id % 2 === 0) {
-                        console.log(chalk.blue(list));
-                    } else {
-                        console.log(list);
-                    }
-                }
-            });
-            sellProducts();
+        }
+        // if this is the first time its ran get a list of products
+        if (runFirstTime) {
+            getListofProducts()
+            // set runFirstTime to false so that it doesnt run again
+            runFirstTime = false;
         }
     });
+}
+
+function getListofProducts() {
+    console.log('\n')
+    // table headers
+    console.log(chalk.magentaBright.underline('ID:\t|\tProduct      \t|\tPrice'))
+    // products data results in unified columns
+    products.forEach(product => {
+        var divider = '\t|\t';
+        var lg_divider = '     \t|\t';
+        var list = "";
+        if (product.product_name.length < 8) {
+            list = product.item_id +  divider + product.product_name + lg_divider + '$' + product.price.toFixed(2);
+        } else if (product.product_name.length > 11) {
+            list = product.item_id +  divider + product.product_name.substring(0, 11) + divider + '$' + product.price.toFixed(2);
+        } else {
+            list = product.item_id +  divider + product.product_name + divider + '$' + product.price.toFixed(2);
+        }
+        
+        // set alternating colors and gray if out of stock
+        if (product.stock_quantity > 0) {
+            if (product.item_id % 2 === 0) {
+                console.log(chalk.blue(list));
+            } else {
+                console.log(list);
+            }
+        } else {
+            console.log(chalk.gray.strikethrough(list + ' OUT OF STOCK'));
+        }
+    });
+    // call sellProducts to start the questions
+    sellProducts();
 }
 
 var updateProducts = function(product_name, amount_purchased, cost) {
     var queryProducts = 'UPDATE products SET stock_quantity = stock_quantity - ? WHERE ?';
     connection.query(queryProducts, [amount_purchased, { product_name: product_name }], function(error, results) {
         if (error) throw error; 
-        console.log('You purchased ' + product_name + ' at a quantity of ' + amount_purchased +
-            '.\nYour total amount charged is $' + cost + '.' );
+        console.log(chalk.greenBright('You purchased ' + product_name + ' at a quantity of ' + amount_purchased +
+            '.\nYour total amount charged is $' + cost.toFixed(2) + '.' ));
+        getProducts();
         buyAgain();
     });
 }
@@ -63,12 +83,14 @@ function sellProducts() {
                 name: 'prod_id',
                 message: chalk.cyan('What is the ID of the product you want to buy?'),
                 validate: function(value) {
-                    if (/[0-9]/i.test(value) && value > 0 && value <= products.length) {
+                    if (products[value-1].stock_quantity < 1) {
+                        return 'Out of stock! Choose another item!';
+                    } else if (/[0-9]/i.test(value) && value > 0 && value <= products.length) {
                         return true;
                     }
                     return 'Enter a valid ID!';
                 }
-            },
+            }
         ]).then(function(id_answer) {
             console.log(chalk.greenBright('\nYou selected ' + products[id_answer.prod_id-1].product_name + '. There are ' + products[id_answer.prod_id-1].stock_quantity + ' available to purchase.'));
             inquirer.prompt(
@@ -97,7 +119,7 @@ function buyAgain() {
         }
     ).then(function(answer) {
         if (answer.again) {
-            getProducts();
+            getListofProducts();
         } else {
             connection.end();
         }
